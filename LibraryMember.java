f54d39e7b8457bea4d4ac059633baf708d82e165
Udara Kavishka <udarakavishka13@gmail.com>
2023-07-13 23:40:04 +0530

    Update LibraryMember.java

--- LibraryMember.java
+++ LibraryMember.java
@@ -1,3 +1,15 @@
 public class LibraryMember{
+	private String memberID;
+	private String name;
+	
+	public LibraryMember(String memberID,String name){
+		this.memberID=memberID;
+		this.name=name;
+	}
+	
+	public String displayMemberDetails(){
+		return "\nMemberID: "+ memberID +
+		"\nName: "+ name;
+	}
 	
 }
