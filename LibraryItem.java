202eb71fcfbbe8125b45cc8133607ca24d2db567
Udara Kavishka <udarakavishka13@gmail.com>
2023-07-13 23:31:03 +0530

    Update LibraryItem.java

--- LibraryItem.java
+++ LibraryItem.java
@@ -1,13 +1,16 @@
 abstract class LibraryItem{
 	protected String title;
 	protected String itemId;
+	protected boolean checked;
 	
 	LibraryItem(String title,
 				String itemId){
 					
 		this.title = title;
 		this.itemId = itemId;
+		this.checked=checked;
 	}
+	
 	public String getTitle() {
 		return title;
 	}
@@ -16,8 +19,25 @@ abstract class LibraryItem{
 		return itemId;
 	}
 	
-	abstract String checkOut();
-	abstract String checkIn();
-	abstract void displayItemDetails();
+	public boolean getCheckOut(){
+		return checked;
+	}
+	
+	public void checkOut(boolean checked) {
+        this.checked = true;
+    }
+	
+	public boolean getCheckIn(){
+		return checked;
+	}
+
+    public void checkIn(boolean checked) {
+        this.checked = false;
+    }
+	
+	
+	public String displayItemDetails() {
+       return "\nTitle: " + title + "\nItem ID: " + itemID;
+    }
 	
 }
