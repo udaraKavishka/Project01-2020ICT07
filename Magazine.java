39114e1ee9c05016c7067ba1af192ad4534ee60c
Udara Kavishka <udarakavishka13@gmail.com>
2023-07-13 23:35:15 +0530

    Update Magazine.java

--- Magazine.java
+++ Magazine.java
@@ -2,24 +2,17 @@ public class Magazine extends LibraryItem{
 	private String issueDate;
 	private String publisher;
 	
-	Magazine(String title,String itemId,String issueDate,String publisher){
+	public Magazine(String title,String itemId,String issueDate,String publisher){
 		super(title,itemId);
 		this.issueDate=issueDate;
 		this.publisher=publisher;
 	}
 	
-	public void displayItemDetails(){
-		System.out.println("Item ID : " + getItemId() +
-		"\n" + "Title : " + getTitle() +
-		"\n" + "Issue date : " + issueDate +
-		"\n" + "Publisher : " + publisher);
-	}
+	public String displayItemDetails(){
+        return super.displayItemDetails() +
+		"\nIssue Date: " + issueDate + 
+		"\nPublisher: " + publisher;
+    }
 	
-	public String checkOut() {
-		return issueDate;
-	}
 	
-	public String checkIn() {
-		return issueDate;
-	}
 }
